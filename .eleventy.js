964a4daf0b034770c7d96dcb047446a4c183dbe7
Chintan <contact@chintanacharya.dev>
2025-12-11 18:29:35 +0530

    ESM

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("CNAME");
